5f8ebe49c269817cdc63d8c31681c12f44c8ca56
Alejandro Marquez <aimarq4670@ung.edu>
2026-07-02 10:36:52 -0400

    Create seed.sql
    Sample sticker data

---
+++ database/seed.sql
@@ -0,0 +1,16 @@
+-- Sample Sticker Data
+
+INSERT INTO stickers (
+    sticker_number,
+    category,
+    title,
+    player_name,
+    country
+)
+VALUES (
+    'MEX-04',
+    'Player',
+    NULL,
+    'Jorge SANCHEZ',
+    'Mexico'
+);
