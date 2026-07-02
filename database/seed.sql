640463b82f4c34d0ab8f293ac2653ff88f21c46d
Alejandro Marquez <aimarq4670@ung.edu>
2026-07-02 13:52:50 -0400

    Update seed.sql
    Adding South Africa sticker data

--- database/seed.sql
+++ database/seed.sql
@@ -22,3 +22,30 @@ VALUES
 ('MEX-18', 'Player', 'Mexico', 'ALEXIS VEGA', 'Mexico'),
 ('MEX-19', 'Player', 'Mexico', 'ROBERTO ALVARADO', 'Mexico'),
 ('MEX-20', 'Player', 'Mexico', 'CESAR HUERTA', 'Mexico');
+
+
+
+-- South Africa Sticker Data
+
+INSERT INTO stickers (sticker_number, category, section, name, country)
+VALUES
+('RSA-01', 'Team Logo', 'South Africa', 'South Africa Team Logo', 'South Africa'),
+('RSA-02', 'Player', 'South Africa', 'RONWEN WILLIAMS', 'South Africa'),
+('RSA-03', 'Player', 'South Africa', 'SIPHO CHAINE', 'South Africa'),
+('RSA-04', 'Player', 'South Africa', 'AUBREY MODIBA', 'South Africa'),
+('RSA-05', 'Player', 'South Africa', 'SAMUKELE KABINI', 'South Africa'),
+('RSA-06', 'Player', 'South Africa', 'MBEKEZELI MBOKAZI', 'South Africa'),
+('RSA-07', 'Player', 'South Africa', 'KHULUMANI NDAMANE', 'South Africa'),
+('RSA-08', 'Player', 'South Africa', 'SIYABONGA NGEZANA', 'South Africa'),
+('RSA-09', 'Player', 'South Africa', 'KHULISO MUDAU', 'South Africa'),
+('RSA-10', 'Player', 'South Africa', 'NKOSINATHI SIBISI', 'South Africa'),
+('RSA-11', 'Player', 'South Africa', 'TEBOHO MOKOENA', 'South Africa'),
+('RSA-12', 'Player', 'South Africa', 'THALENTE MBATHA', 'South Africa'),
+('RSA-13', 'Team Sticker', 'South Africa', 'South Africa Team Sticker', 'South Africa'),
+('RSA-14', 'Player', 'South Africa', 'BATHUSI AUBAAS', 'South Africa'),
+('RSA-15', 'Player', 'South Africa', 'YAYA SITHOLE', 'South Africa'),
+('RSA-16', 'Player', 'South Africa', 'SIPHO MBULE', 'South Africa'),
+('RSA-17', 'Player', 'South Africa', 'LYLE FOSTER', 'South Africa'),
+('RSA-18', 'Player', 'South Africa', 'IQRAAM RAYNERS', 'South Africa'),
+('RSA-19', 'Player', 'South Africa', 'MOHAU NKOTA', 'South Africa'),
+('RSA-20', 'Player', 'South Africa', 'OSWIN APPOLLIS', 'South Africa');
