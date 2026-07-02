69222c7740dca670dde3250184f440dbe1dfdfa0
Alejandro Marquez <aimarq4670@ung.edu>
2026-07-02 13:18:51 -0400

    Update seed.sql
    Adding Mexico sticker data

--- database/seed.sql
+++ database/seed.sql
@@ -1,14 +1,24 @@
--- Sample Sticker Data
+-- Mexico Sticker Data
 
-INSERT INTO stickers (
-    sticker_number,
-    category,
-    section,
-    name,
-    country
-)
-VALUES (
+INSERT INTO stickers (sticker_number, category, section, name, country)
+VALUES
 ('MEX-01', 'Team Logo', 'Mexico', 'Mexico Team Logo', 'Mexico'),
-('MEX-04', 'Player', 'Mexico', 'Jorge SANCHEZ', 'Mexico'),
+('MEX-02', 'Player', 'Mexico', 'LUIS MALAGON', 'Mexico'),
+('MEX-03', 'Player', 'Mexico', 'JOHAN VASQUEZ', 'Mexico'),
+('MEX-04', 'Player', 'Mexico', 'JORGE SANCHEZ', 'Mexico'),
+('MEX-05', 'Player', 'Mexico', 'CESAR MONTES', 'Mexico'),
+('MEX-06', 'Player', 'Mexico', 'JESUS GALLARDO', 'Mexico'),
+('MEX-07', 'Player', 'Mexico', 'ISRAEL REYES', 'Mexico'),
+('MEX-08', 'Player', 'Mexico', 'DIEGO LAINEZ', 'Mexico'),
+('MEX-09', 'Player', 'Mexico', 'CARLOS RODRIGUEZ', 'Mexico'),
+('MEX-10', 'Player', 'Mexico', 'EDSON ALVAREZ', 'Mexico'),
+('MEX-11', 'Player', 'Mexico', 'ORBELIN PINEDA', 'Mexico'),
+('MEX-12', 'Player', 'Mexico', 'MARCEL RUIZ', 'Mexico'),
 ('MEX-13', 'Team Sticker', 'Mexico', 'Mexico Team Sticker', 'Mexico'),
-('FWCB-01', 'Match Ball', 'Intro', 'Official Match Ball', NULL);
+('MEX-14', 'Player', 'Mexico', 'ERICK SANCHEZ', 'Mexico'),
+('MEX-15', 'Player', 'Mexico', 'HIRVING LOZANO', 'Mexico'),
+('MEX-16', 'Player', 'Mexico', 'SANTIAGO GIMENEZ', 'Mexico'),
+('MEX-17', 'Player', 'Mexico', 'RAUL JIMENEZ', 'Mexico'),
+('MEX-18', 'Player', 'Mexico', 'ALEXIS VEGA', 'Mexico'),
+('MEX-19', 'Player', 'Mexico', 'ROBERTO ALVARADO', 'Mexico'),
+('MEX-20', 'Player', 'Mexico', 'CESAR HUERTA', 'Mexico');
